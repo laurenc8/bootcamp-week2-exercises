4da87481799c8b28b266bbd7552c62b04b595f7a
laurenc8 <laurenc8822@gmail.com>
2021-01-13 23:23:19 -0500

    03_patch

--- objection-exercises/src/exercises/02_insert.js
+++ objection-exercises/src/exercises/02_insert.js
@@ -1,12 +1,27 @@
 const cleanup = require('../lib/cleanup')
+const Pet = require('../models/Pet')
 // Import models
+const User = require('../models/User')
 
 const run = async () => {
   // Write Queries and Logs Here !!!
 
   // Insert yourself in the users table
+  const insertMe = await User.query().insert({
+    email: '022063@gmail.com',
+    firstName: 'Lauren',
+    lastName: 'Chen',
+    age: 17
+  })
+  console.log(insertMe)
 
   // Insert a pet belonging to you (get your ID from Postico or DBeaver)
+  // const insertMyPet = await Pet.query().insert({
+  //   ownerId: ,
+  //   type: FISH,
+  //   name: 'Henry'
+  // })
+  // console.log(insertMyPet)
 
   // -----
   cleanup()
--- objection-exercises/src/exercises/03_patch.js
+++ objection-exercises/src/exercises/03_patch.js
@@ -1,10 +1,15 @@
 const cleanup = require('../lib/cleanup')
 // Import models
+const User = require('../models/User')
 
 const run = async () => {
   // Write Queries and Logs Here !!!
 
   // Update anyone below the age of 18 to be 18 years old (they shouldn't be keeping pets)
+  const legal = await User.query().patch({
+    age: 18
+  }).where('age', '<', 18)
+  console.log(legal)
 
   // -----
   cleanup()
