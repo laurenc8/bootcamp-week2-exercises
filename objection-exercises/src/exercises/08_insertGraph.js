6f3cf712002d35ab2af538b5e19e2d7efbc1dcec
laurenc8 <laurenc8822@gmail.com>
2021-01-14 18:42:28 -0500

    08_insertGraph

--- objection-exercises/src/exercises/08_insertGraph.js
+++ objection-exercises/src/exercises/08_insertGraph.js
@@ -1,10 +1,30 @@
+const { userParams } = require('../lib')
 const cleanup = require('../lib/cleanup')
 // Import models
+const User = require('../models/User')
 
 const run = async () => {
   // Write Queries and Logs Here !!!
 
   // Insert a new person name Peter Bynum with two pet DOGs named Rocco & Rosey
+
+  const peterGraph = await User.query().insertGraph({
+    email: 'peter.bynum@gmail.com',
+    firstName: 'Peter',
+    lastName: 'Bynum',
+
+    pets: [
+      {
+        type: 'DOG',
+        name: 'Rocco'
+      },
+      {
+        type: 'DOG',
+        name: 'Rosey'
+      }
+    ]
+  })
+  console.log(peterGraph)
 
   // -----
   cleanup()
