86b32a19c0e3d8937dbbd0e023e4486bc10ae28e
laurenc8 <laurenc8822@gmail.com>
2021-01-13 23:34:47 -0500

    06_aggregate

--- objection-exercises/src/exercises/02_insert.js
+++ objection-exercises/src/exercises/02_insert.js
@@ -1,7 +1,7 @@
 const cleanup = require('../lib/cleanup')
-const Pet = require('../models/Pet')
 // Import models
 const User = require('../models/User')
+const Pet = require('../models/Pet')
 
 const run = async () => {
   // Write Queries and Logs Here !!!
--- objection-exercises/src/exercises/06_aggregate.js
+++ objection-exercises/src/exercises/06_aggregate.js
@@ -1,14 +1,22 @@
 const cleanup = require('../lib/cleanup')
 // Import models
+const User = require('../models/User')
+const Pet = require('../models/Pet')
 
 const run = async () => {
   // Write Queries and Logs Here !!!
 
   // Get the total number of users
+  const countUsers = await User.query().resultSize()
+  console.log(countUsers)
 
   // Get the average age of users
+  const avgAge = await User.query().avg('age as avgAge')
+  console.log(avgAge)
 
   // Get the total number of dogs
+  const countDogs = await Pet.query().where('type', 'DOG').resultSize()
+  console.log(countDogs)
 
   // -----
   cleanup()
