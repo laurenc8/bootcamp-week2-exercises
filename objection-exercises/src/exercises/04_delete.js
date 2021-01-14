0319fd54852ec314c8f5cb01968ca488a2f77bce
laurenc8 <laurenc8822@gmail.com>
2021-01-13 23:25:49 -0500

    04_delete

--- objection-exercises/src/exercises/04_delete.js
+++ objection-exercises/src/exercises/04_delete.js
@@ -1,10 +1,13 @@
 const cleanup = require('../lib/cleanup')
 // Import models
+const Pet = require('../models/Pet')
 
 const run = async () => {
   // Write Queries and Logs Here !!!
 
   // Delete all CATS
+  const noCats = await Pet.query().delete().where('type', 'CAT')
+  console.log(noCats)
 
   // -----
   cleanup()
