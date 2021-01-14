df202b3300e31c3b97b40f7ecfb0397e658a26f3
laurenc8 <laurenc8822@gmail.com>
2021-01-13 23:28:43 -0500

    05_limitoffset

--- objection-exercises/src/exercises/05_limitoffset.js
+++ objection-exercises/src/exercises/05_limitoffset.js
@@ -1,12 +1,17 @@
 const cleanup = require('../lib/cleanup')
 // Import models
+const User = require('../models/User')
 
 const run = async () => {
   // Write Queries and Logs Here !!!
 
   // Get the first 5 users, ordered by lastName
+  const firstFive = await User.query().orderBy('lastName').limit(5)
+  console.log(firstFive)
 
   // Get the second 5 users, ordered by lastName
+  const nextFive = await User.query().orderBy('lastName').limit(5).offset(5)
+  console.log(nextFive)
 
   // -----
   cleanup()
