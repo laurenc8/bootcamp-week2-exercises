94dbbd87564b98f20c459f543738fedae09070d8
laurenc8 <laurenc8822@gmail.com>
2021-01-13 22:12:34 -0500

    00_select

--- objection-exercises/src/models/Relation.js
+++ objection-exercises/src/models/Relation.js
@@ -1,1 +1,28 @@
-// Write your relation model here!
+const { ManyToManyRelation } = require('./BaseModel')
+const BaseModel = require('./BaseModel')
+
+class Relation extends BaseModel {
+  static get tableName() {
+    return 'relations'
+  }
+
+  static get relationMappings() {
+    const User = require('./User')
+    return {
+      users: {
+        relation: ManyToManyRelation,
+        modelClass: User,
+        join: {
+          from: 'relations.parentId',
+          through: {
+            from: 'users.id',
+            to: 'users.id',
+          },
+          to: 'relations.childId',
+        },
+      },
+    }
+  }
+}
+
+module.exports = Relation
--- objection-exercises/src/models/User.js
+++ objection-exercises/src/models/User.js
@@ -1,3 +1,4 @@
+const { HasManyRelation, ManyToManyRelation } = require('./BaseModel')
 const BaseModel = require('./BaseModel')
 
 class User extends BaseModel {
@@ -6,7 +7,42 @@ class User extends BaseModel {
   }
 
   static get relationMappings() {
-    return {}
+    const Pet = require('./Pet')
+    const Relation = require('./Relation')
+    return {
+      pets: {
+        relation: HasManyRelation,
+        modelClass: Pet,
+        join: {
+          from: 'users.id',
+          to: 'pets.ownerId',
+        },
+      },
+      relations: {
+        relation: ManyToManyRelation,
+        modelClass: Relation,
+        join: {
+          from: 'users.id',
+          through: {
+            from: 'relations.parentId',
+            to: 'relations.childId',
+          },
+          to: 'users.id',
+        },
+      },
+      // parents: {
+      //   relation: ManyToManyRelation,
+      //   modelClass: Relation,
+      //   join: {
+      //     from: 'users.id',
+      //     through: {
+      //       from: 'relations.childId',
+      //       to: 'relations.parentId',
+      //     },
+      //     to: 'users.id',
+      //   },
+      // },
+    }
   }
 }
 
